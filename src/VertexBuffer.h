0f08f1913291179eaf75d0e5b1d809bdf8b20cdd
Ritaban Chaudhuri <inrcr27@gmail.com>
2024-07-12 10:29:41 +0530

    Added Vertex Buffer

---
+++ src/VertexBuffer.h
@@ -0,0 +1,14 @@
+#pragma once
+
+class VertexBuffer
+{
+private:
+	unsigned int m_RendererID;
+public:
+	VertexBuffer(const void* data, unsigned int size);
+	~VertexBuffer();
+
+	void bind() const;
+	void unBind() const;
+
+};
